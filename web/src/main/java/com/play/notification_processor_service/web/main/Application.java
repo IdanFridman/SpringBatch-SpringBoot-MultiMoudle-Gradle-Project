7794b83a4cb9d8ef5d276130e37cdba3866ac48a
idan <idan.frid@gmail.com>
2015-01-15 14:50:15 +0200

    fixed tests

---
+++ web/src/main/java/com/play/notification_processor_service/web/main/Application.java
@@ -0,0 +1,14 @@
+package com.play.notification_processor_service.web.main;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by idan on 1/15/15.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.printf("Started the app");
+    }
+}
